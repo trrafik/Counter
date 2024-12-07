6130234d7cc47a66fcc2a07e126077d2f094b66d
trrafik <trrafik29@yandex.ru>
2024-12-07 06:26:32 +0300

    Реализована Базовая задача

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var counter: Int = 0
+    @IBOutlet weak var counterLabelView: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func countButtonDidTap(_ sender: Any) {
+        counter += 1
+        counterLabelView.text = String(counter)
+
+    }
 }
 
