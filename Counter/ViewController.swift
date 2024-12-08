6fb3c1a3f23286ef42ea4b44955f6d57a85d2d2b
trrafik <trrafik29@yandex.ru>
2024-12-08 17:13:28 +0300

    Внесены правки по рекомендациям ревью

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,15 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
     private var counter: Int = 0
-    @IBOutlet weak var counterLabelView: UILabel!
-    @IBOutlet weak var historyTextView: UITextView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    @IBOutlet private weak var counterLabelView: UILabel!
+    @IBOutlet private weak var historyTextView: UITextView!
+    
     private func createDateTimeString() -> String {
         let currentDateTime = Date()
         let formatter = DateFormatter()
@@ -23,13 +20,14 @@ class ViewController: UIViewController {
         let currentDateTimeString = formatter.string(from: currentDateTime)
         return currentDateTimeString
     }
-
-    @IBAction func plusButtonDidTap(_ sender: Any) {
+    
+    @IBAction private func plusButtonDidTap(_ sender: Any) {
         counter += 1
         counterLabelView.text = String(counter)
         historyTextView.text += "\n[\(createDateTimeString())]: значение изменено на +1"
     }
-    @IBAction func minusButtonDidTap(_ sender: Any) {
+    
+    @IBAction private func minusButtonDidTap(_ sender: Any) {
         if counter > 0 {
             counter -= 1
             counterLabelView.text = String(counter)
@@ -39,11 +37,10 @@ class ViewController: UIViewController {
             historyTextView.text += "\n[\(createDateTimeString())]: попытка уменьшить значение счётчика ниже 0"
         }
     }
-    @IBAction func resetButtonDidTap(_ sender: Any) {
+    
+    @IBAction private func resetButtonDidTap(_ sender: Any) {
         counter = 0
         counterLabelView.text = String(counter)
         historyTextView.text += "\n[\(createDateTimeString())]: значение сброшено"
     }
-    
 }
-
