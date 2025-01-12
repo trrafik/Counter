ad713b6457b1e3ac1e837f0b3cffa8e9d83d8cbd
trrafik <trrafik29@yandex.ru>
2025-01-12 16:08:18 +0300

    Сохранение значения счетчика и истории в UserDefaults в рамках 5-го спринта

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -13,18 +13,16 @@ final class ViewController: UIViewController {
     @IBOutlet private weak var counterLabelView: UILabel!
     @IBOutlet private weak var historyTextView: UITextView!
     
-    private func createDateTimeString() -> String {
-        let currentDateTime = Date()
-        let formatter = DateFormatter()
-        formatter.dateFormat = "dd.MM.yyyy HH:mm"
-        let currentDateTimeString = formatter.string(from: currentDateTime)
-        return currentDateTimeString
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        returnInfoFromUserDefaults()
     }
     
     @IBAction private func plusButtonDidTap(_ sender: Any) {
         counter += 1
         counterLabelView.text = String(counter)
         historyTextView.text += "\n[\(createDateTimeString())]: значение изменено на +1"
+        saveInfoToUserDefaults(counter: counter, historyText: historyTextView.text)
     }
     
     @IBAction private func minusButtonDidTap(_ sender: Any) {
@@ -36,11 +34,33 @@ final class ViewController: UIViewController {
         else {
             historyTextView.text += "\n[\(createDateTimeString())]: попытка уменьшить значение счётчика ниже 0"
         }
+        saveInfoToUserDefaults(counter: counter, historyText: historyTextView.text)
     }
     
     @IBAction private func resetButtonDidTap(_ sender: Any) {
         counter = 0
         counterLabelView.text = String(counter)
         historyTextView.text += "\n[\(createDateTimeString())]: значение сброшено"
+        saveInfoToUserDefaults(counter: counter, historyText: historyTextView.text)
     }
+    
+    private func createDateTimeString() -> String {
+        let currentDateTime = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy HH:mm"
+        let currentDateTimeString = formatter.string(from: currentDateTime)
+        return currentDateTimeString
+    }
+    
+    private func saveInfoToUserDefaults(counter: Int, historyText: String) {
+        UserDefaults.standard.set(counter, forKey: "counterInt")
+        UserDefaults.standard.set(historyText, forKey: "historyTextString")
+    }
+    
+    private func returnInfoFromUserDefaults() {
+        counter = UserDefaults.standard.integer(forKey: "counterInt")
+        counterLabelView.text = String(counter)
+        historyTextView.text = UserDefaults.standard.string(forKey: "historyTextString") ?? ""
+    }
+    
 }
